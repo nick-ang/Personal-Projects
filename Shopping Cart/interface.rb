7697f4e419831772be8fb7d7a5fc371de69cceef
Nick Ang <nicholas_ang@outlook.com>
2021-07-25 21:20:20 +1000

    Updated code

--- Shopping Cart/interface.rb
+++ Shopping Cart/interface.rb
@@ -1,4 +1,4 @@
-# TODO: you can build your christmas list program here!
+
 require 'open-uri'
 require 'nokogiri'
 require 'csv'
@@ -29,7 +29,7 @@ until choice == "quit"
   choice = gets.chomp
   case choice
   when "list"
-    # TODO: list items
+    # list items
     items.each do |item|
       puts "#{items.index(item) + 1} - [#{hash[item]}] #{item}"
     end
@@ -40,12 +40,12 @@ until choice == "quit"
     puts "What are you searching on Etsy?"
     article = gets.chomp
 
-    # 1. We get the HTML page content thanks to open-uri
+    # 1. Get the HTML page content thanks to open-uri
     html_content = URI.open("https://www.etsy.com/search?q=#{article}").read
     # 2. We build a Nokogiri document from this file
     doc = Nokogiri::HTML(html_content)
 
-    # 3. We search for the correct elements containing the items' title in our HTML doc
+    # 3. Search for the correct elements containing the items' title in our HTML doc
     doc.search('.v2-listing-card .v2-listing-card__info .text-body').each do |element|
       # 4. For each item found, we extract its title and print it
       catalog << element
@@ -56,13 +56,13 @@ until choice == "quit"
     items << catalog[index - 1].text.strip
     hash[catalog[index - 1].text.strip] = " "
     save_csv(item, "true")
-    # TODO: ask user an item and add it to gift list
+    # Ask user an item and add it to gift list
   when "add"
     puts "Enter an item"
     item = gets.chomp
     items << item
     hash[item] = " "
-    # TODO: ask user the index of item to delete and delete it
+    # Ask user the index of item to delete and delete it
   when "mark"
     puts "Which item have you bought (give the index)?"
     index = gets.chomp.to_i
@@ -80,8 +80,7 @@ end
   filepath    = 'gifts.csv'
 
   CSV.open(filepath, 'wb', csv_options) do |csv|
-    # We had headers to the CSV
-    #TODO: store each gift
+    # Store each gift
     puts "saved to CSV file"
     end
 
@@ -90,9 +89,8 @@ end
   filepath    = 'gifts.csv'
 
   CSV.open(filepath, 'a', csv_options) do |csv|
-    # We had headers to the CSV
     csv << [items[n], hash[items[n]]]
-    #TODO: store each gift
+    # store each gift
   end
 end
 
--- Shopping Cart/scraper.rb
+++ Shopping Cart/scraper.rb
@@ -2,13 +2,13 @@
 require 'nokogiri'
 
 filepath = "results.html"
-# 1. We get the HTML page content
+# Get the HTML page content
 html_content = File.open(filepath)
-# 2. We build a Nokogiri document from this file
+# Build a Nokogiri document from this file
 doc = Nokogiri::HTML(html_content)
 
-# 3. We search for the correct elements containing the items' title in our HTML doc
+# Search for the correct elements containing the items' title in our HTML doc
 doc.search('.v2-listing-card .v2-listing-card__info .text-body').each do |element|
-  # 4. For each item found, we extract its title and print it
+  #For each item found, extract its title and print it
   puts element.text.strip
 end
